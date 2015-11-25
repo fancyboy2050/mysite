a7abc83528c24cf5a729107f54d929e7c2608d8c
fancyboy2050 <tfancyboy@gmail.com>
2015-11-25 14:31:45 +0800

    Update Stat.py
    update

--- test/Stat.py
+++ test/Stat.py
@@ -3,7 +3,7 @@ from MySqlConnect import Connect
 import mysql.connector as sqlConnector
 
 try:
-    connector = Connect(user="bookAdmin", password="bookAdmin", database="173", host="10.3.254.97", port="3307")
+    connector = Connect(user="bookAdmin", password="bookAdmin", database="173", host="127.0.0.1", port="3307")
     result = connector.select(table="mobile_app", limit="5");
     for res in result:
         print(res)
